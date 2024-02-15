ed62c657e6c77dc3a075053e082639200e1e00bb
windleaf <yunhe9337@gmail.com>
2024-02-15 14:26:38 +0800

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "新叶的恋爱推理笔记",
+  tagline: "经验之谈，收集关于恋爱、相亲案例的个人向信息站",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://loveriddleswithnewleaf.cyou/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "新叶的恋爱推理笔记",
+        logo: {
+          alt: "恋爱推理笔记图标",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "恋爱理论",
+          },
+          {
+            position: "left",
+            label: "爱情、婚礼歌曲",
+            to: "/music",
+          },
+          {
+            position: "left",
+            label: "小游戏",
+            to: "/game",
+          },
+          { to: "/blog", label: "日志", position: "right" },
+          { to: "/about", label: "关于我", position: "right" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            label: "推理笔记",
+            to: "/docs/intro",
+          },
+          {
+            label: "爱情、婚礼歌曲",
+            href: "/music",
+          },
+          {
+            label: "小游戏",
+            href: "/game",
+          },
+          {
+            label: "日志",
+            to: "/blog",
+          },
+          {
+            label: "关于我",
+            to: "/about",
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
